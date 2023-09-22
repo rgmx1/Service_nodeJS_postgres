35257113a2d790ecf658374fa2794b6176c5895f
RGMX1 <kef1rgreece@gmail.com>
2023-09-22 20:37:26 +0500

    create table

--- database.sql
+++ database.sql
@@ -4,11 +4,4 @@ name VARCHAR(255),
 surname VARCHAR(255)
 );
 
-
-create table pet(
-id SERIAL PRIMARY KEY,
-name VARCHAR(255),
-age INTEGER,
-user_id INTEGER,
-FOREIGN KEY (user_id) REFERENCES persone (id)
-);
+// создание таблицы в бд
